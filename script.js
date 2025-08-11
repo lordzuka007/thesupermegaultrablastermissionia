c797b562f81c8b5a0b1f74015be3e4cffa5e9e7f
lordzuka007 <135721165+lordzuka007@users.noreply.github.com>
2025-08-11 11:50:42 -0300

    a2

---
+++ script.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector(".caixa-Principal");
+const caixaPergunta = document.querySelector(".caixa-Pergunta");
+const caixaAlternativa = document.querySelector(".caixa-Alternativa");
+const caixaResultado = document.querySelector(".caixa-Resultado");
+const Textoresultado = document.querySelector(".Texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            "alternativas 1",
+            "alternativa 2"]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "alternativas 1",
+            "alternativa 2"]
+    }
+]
