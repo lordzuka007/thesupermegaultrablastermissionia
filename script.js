0326d64db06940fb344ad875d78214f43d0d9098
lordzuka007 <135721165+lordzuka007@users.noreply.github.com>
2025-08-25 11:58:53 -0300

    TOBE CONTUNUE

--- script.js
+++ script.js
@@ -6,15 +6,37 @@ const Textoresultado = document.querySelector(".Texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "pergunta 1",
-        alternativas: [
-            "alternativas 1",
-            "alternativa 2"]
+        enunciado: "Em que mapa do battle royale Warzone original o gulag estava localizado?",
+        alternativas: 
+            "Caldera",
+            "Verdansk"
     },
     {
-        enunciado: "pergunta 2",
-        alternativas: [
-            "alternativas 1",
-            "alternativa 2"]
-    }
-]
+        enunciado: "Qual é o objetivo principal de um jogador que entra no gulag?",
+        alternativas: 
+            "Reviver um companheiro de equipe caído.",
+            "Lutar contra outro jogador para retornar ao jogo."
+    },
+{
+        enunciado: "Que item permite que um jogador compre equipamentos, reavive companheiros de equipe e obtenha loadouts?",
+        alternativas: 
+            "Dinheiro do jogo (Cash)",
+            "Placas de blindagem"
+    },
+{
+        enunciado: "Qual é a designação para o círculo de gás que encolhe gradualmente ao longo de uma partida?",
+        alternativas: 
+            "A zona",
+            "O limbo"
+    },
+{
+        enunciado: "Qual é o nome da função que permite a um jogador obter seu loadout personalizado no Warzone?",
+        alternativas: 
+            "Caixa de munição (Ammo Box)",
+            "Entrega de armamento (Loadout Drop)"
+    };
+];
+let Atual =0;
+let perguntaAtual;
+
+function (mostraPergunta)
